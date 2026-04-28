426e724641c662bef9f3aee2954689e23dd7949f
jun28th <doanlevy2806@gmail.com>
2026-04-29 01:21:47 +0700

    fix some UI

--- app/login.tsx
+++ app/login.tsx
@@ -53,80 +53,11 @@ export default function LoginScreen() {
 
   return (
     <View className="flex-1 bg-white">
-<<<<<<< Updated upstream
-        <SafeAreaView edges={["top"]} className="bg-purple" />
-=======
       <SafeAreaView edges={["top"]} className="bg-gwhite" />
->>>>>>> Stashed changes
 
       <SafeAreaView edges={["left", "right"]} className="flex-1 bg-white">
         <Header />
 
-<<<<<<< Updated upstream
-            <View className="flex-1 items-center justify-center">
-                <View className="w-full bg-white gap-5 p-5">
-
-
-                    <View className="items-center mb-8">
-                        <Image
-                        source={require("@/public/assets/image/loginImg.png")}
-                        className="w-1/2 h-[131px]"
-                        resizeMode="cover"
-                        />
-                    </View>
-
-                    <Text className="text-center font-semibold text-purple text-lg">
-                        ĐĂNG NHẬP
-                    </Text>
-
-                    <View className="gap-5">
-                        <View>
-                            <Text className="text-sm text-tgray9 mb-2">Tên đăng nhập</Text>
-                            <TextInput
-                                placeholder="Điền tên đăng nhập"
-                                placeholderTextColor="#808080"
-                                value={username}
-                                onChangeText={setUsername}
-                                className="h-12 mt-1 px-3 py-2 border border-tgray5 rounded-lg text-tgray5"
-                            />
-                        </View>
-
-                        <View>
-                            <Text className="text-sm text-tgray9 mb-2">Mật khẩu</Text>
-                            <TextInput
-                                placeholder="Điền mật khẩu"
-                                placeholderTextColor="#808080"
-                                value={password}
-                                onChangeText={setPassword}
-                                secureTextEntry
-                                className=" h-12 mt-1 px-3 py-2 border border-tgray5 rounded-lg text-tgray5"
-                            />
-                        </View>
-                        
-                        {error ? (
-                        <View className="mb-2 p-2">
-                            <Text className="text-red font-semibold text-sm text-center">{error}</Text>
-                        </View>
-                        ) : null}
-
-                        <View className="items-center">
-                            <TouchableOpacity
-                                onPress={handleLogin}
-                                disabled={loginMutation.isPending}
-                                className="p-3 rounded-xl items-center bg-pink "
-                            >
-                                {loginMutation.isPending ? (
-                                <ActivityIndicator color="white" />
-                                ) : (
-                                <Text className="text-white text-sm font-semibold">Đăng nhập</Text>
-                                )}
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-
-                </View>
-                
-=======
         <View className="flex-1 items-center ">
           <View className="w-full bg-white gap-5 p-5">
             <View className="items-center mb-8 py-5">
@@ -135,7 +66,6 @@ export default function LoginScreen() {
                 className="w-1/2 h-[131px]"
                 resizeMode="cover"
               />
->>>>>>> Stashed changes
             </View>
 
             <Text className="text-center font-semibold text-purple text-lg">
